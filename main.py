2c987a4fe772d6398c64988d9197c69a0febfa5a
EMajdalani <EMajdalani121@gmail.com>
2025-03-28 22:55:43 -0400

    player can rotate L/R

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
@@ -22,7 +23,7 @@ def main():
         
         screen.fill((0, 0, 0))
         
-        player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+        player.update(dt)
         player.draw(screen)
 
         pygame.display.flip()
