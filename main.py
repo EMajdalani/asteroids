a9a4193f8ee5e395f55b9a41d256d576d0c68672
EMajdalani <emajdalani121@gmail.com>
2026-08-19 23:14:54 -0400

    added clock

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
 
     while True:
         log_state()
@@ -13,6 +15,7 @@ def main():
                 return
         screen.fill("Black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
